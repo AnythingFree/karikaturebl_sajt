5215dd2079b09db48f30271341ec29cdd456a589
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2024-08-22 00:35:11 +0200

    sredila navigaciju. izbacila linkove na dnu. dodala back to top dugme. sredjivala 'porucite' sekciju.

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,5 +184,22 @@
 			}, 275);
 
 		});
+		
+	// BACK TO TOP BUTTON
+	// Get the button:
+	let mybutton = document.getElementById("scrollToTopBtn");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+	}
+
+	
 
 })(jQuery);
