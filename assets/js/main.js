20e018a406f9079505dc330d62ffd71448e425e9
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2024-12-20 21:18:43 +0100

    Dodala formu da ubaci info koje zeli kad narucuje neki paket.

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,11 +193,11 @@
 	window.onscroll = function() {scrollFunction()};
 
 	function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		mybutton.style.display = "block";
-	} else {
-		mybutton.style.display = "none";
-	}
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			mybutton.style.display = "block";
+		} else {
+			mybutton.style.display = "none";
+		}
 	}
 
 	
