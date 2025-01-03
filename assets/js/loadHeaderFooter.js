92cab3d26e0f01b9100d3f294d992bc8ba4683ed
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2025-01-03 15:16:37 +0100

    footer i headre odvojeni i jos ponesto

---
+++ assets/js/loadHeaderFooter.js
@@ -0,0 +1,19 @@
+// Function to dynamically load the header and footer
+async function loadComponent(id, filePath) {
+    try {
+        const response = await fetch(filePath);
+        if (response.ok) {
+            const content = await response.text();
+            document.getElementById(id).innerHTML = content;
+        } else {
+            console.error(`Failed to load ${filePath}: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(`Error loading ${filePath}:`, error);
+    }
+}
+
+// Load the header and footer
+loadComponent("header-placeholder", "header.html");
+loadComponent("footer-placeholder", "footer.html");
+loadComponent("scrollTopButton-placeholder", "scrollTopButton.html");
