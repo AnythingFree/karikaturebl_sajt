0d70122109e59aaba402266fc7a6198d9cb4c78c
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2025-12-07 02:19:01 +0100

    fix: header footer and orders loading

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,29 +1,31 @@
-// SLIDERSHOW
-let slideIndex = 1;
-showSlides(slideIndex);
+function initSlideshow() {
+    let slideIndex = 1;
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    function showSlides(n) {
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (slides.length === 0) return; // prevent errors
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+    window.plusSlides = function(n) {
+        showSlides(slideIndex += n);
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-} 
+
+    window.currentSlide = function(n) {
+        showSlides(slideIndex = n);
+    }
+
+    showSlides(slideIndex);
+}
